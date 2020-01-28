901f01a615d7da71c831225e4ac475119513d3ee
Etienne Pestre <pestree@f211-10.e-im2ag.ujf-grenoble.fr>
2020-01-28 11:29:43 +0100

    pro lvl3

--- code/test/test_thread_prod_cons.c
+++ code/test/test_thread_prod_cons.c
@@ -22,20 +22,18 @@ void producteur(parametre* par){
     SynchPutString("Debut Producteur:",30);
     SynchPutInt(par->id);
     PutChar('\n');
-    
-    if(*par->nbPlaces<par->maxPlace){
-      (*par->nbPlaces)++;
-      int a;
-      for(a=0;a<*par->nbCAtt;a++){
-        V(par->consomateurAtt);
-      }
-      *par->nbCAtt=0;
-    }else{
-      V(par->verrou);
+    while(!(*par->nbPlaces<par->maxPlace)){
       (*par->nbPAtt)++;
+      V(par->verrou);
       P(par->producteurAtt);
       P(par->verrou);
     }
+    (*par->nbPlaces)++;
+    int a;
+    for(a=0;a<*par->nbCAtt;a++){
+      V(par->consomateurAtt);
+    }
+    *par->nbCAtt=0;
     SynchPutString("Fin Producteur:",30);
     SynchPutInt(par->id);
     PutChar('\n');
@@ -53,19 +51,18 @@ void consomateur(parametre* par){
     SynchPutString("Debut Consomateur:",30);
     SynchPutInt(par->id);
     PutChar('\n');
-    if(*par->nbPlaces>0){
-      (*par->nbPlaces)--;
-      int a;
-      for(a=0;a<*par->nbPAtt;a++){
-        V(par->producteurAtt);
-      }
-      *par->nbPAtt=0;
-    }else{
-      V(par->verrou);
+    while(!(*par->nbPlaces>0)){
       (*par->nbCAtt)++;
+      V(par->verrou);
       P(par->consomateurAtt);
       P(par->verrou);
     }
+    (*par->nbPlaces)--;
+    int a;
+    for(a=0;a<*par->nbPAtt;a++){
+      V(par->producteurAtt);
+    }
+    *par->nbPAtt=0;
     SynchPutString("Fin Consomateur:",30);
     SynchPutInt(par->id);
     PutChar('\n');
@@ -91,17 +88,20 @@ int main(){
   //////////////////////////////////////////////////////////////////////////////
   //lance
   //////////////////////////////////////////////////////////////////////////////
-  for(;nbThread<10;nbThread++){
+  for(;nbThread<25;nbThread++){
     //SynchPutInt(nbThread);
     parametre p={nbThread,5,&nbPlaces,verrou,&nbPAtt,producteurAtt,&nbCAtt,consomateurAtt};
     parametres[nbThread]=p;
     tab[nbThread]=UserThreadCreate((void (*)(void *))producteur,(void *)&parametres[nbThread]);
+    SynchPutString("lance::",30);
   }
-  for(;nbThread<20;nbThread++){
+  for(;nbThread<50;nbThread++){
     parametre p={nbThread,5,&nbPlaces,verrou,&nbPAtt,producteurAtt,&nbCAtt,consomateurAtt};
     parametres[nbThread]=p;
     tab[nbThread]=UserThreadCreate((void (*)(void *))consomateur,(void *)&parametres[nbThread]);
+    SynchPutString("lance::",30);
   }
+  SynchPutString("Fin Lancer!!!!!!!!",30);
   //////////////////////////////////////////////////////////////////////////////
   //Attente des thread lancer durant le test
   //////////////////////////////////////////////////////////////////////////////
